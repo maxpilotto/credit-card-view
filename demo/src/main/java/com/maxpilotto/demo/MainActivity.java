c2f96afa28e1157f5aa5f783fa0f72e22e36c370
maxpilotto <maxpil99@outlook.com>
2019-06-20 14:31:24 +0200

    migrate project to AndroidX

--- demo/src/main/java/com/maxpilotto/demo/MainActivity.java
+++ demo/src/main/java/com/maxpilotto/demo/MainActivity.java
@@ -1,6 +1,6 @@
 package com.maxpilotto.demo;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
